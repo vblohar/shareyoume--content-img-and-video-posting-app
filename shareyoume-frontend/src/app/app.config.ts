a4217e7cb578983a418da0971aaf29c3b1245247
Vaibhav L <loharvb@gmail.com>
2025-04-24 17:43:39 +0530

    Added Jwt token into Http headers..

--- shareyoume-frontend/src/app/app.config.ts
+++ shareyoume-frontend/src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptorFn } from './components/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideHttpClient(),
-    
+    provideHttpClient(withInterceptors([authInterceptorFn])),
+          
   ]
 };
+
+
--- shareyoume-frontend/src/app/components/interceptors/auth.interceptor.ts
+++ shareyoume-frontend/src/app/components/interceptors/auth.interceptor.ts
@@ -1,26 +1,18 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { TokenStorageService } from '../services/token-storage.service';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  constructor(private tokenStorage: TokenStorageService) {}
+export const authInterceptorFn: HttpInterceptorFn = (req, next) => {
+  const tokenStorage = inject(TokenStorageService);
+  const token = tokenStorage.getToken();
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = this.tokenStorage.getToken();
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    }
-    return next.handle(request);
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
-}
+
+  return next(req);
+};
