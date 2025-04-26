cf363f72c7ac2109a6e643a5d6372103ce3024d3
Vaibhav L <loharvb@gmail.com>
2025-04-26 20:47:40 +0530

    Created editprofile component..

---
+++ shareyoume-frontend/src/app/components/profile/editprofile/editprofile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editprofile',
+  imports: [],
+  templateUrl: './editprofile.component.html',
+  styleUrl: './editprofile.component.css'
+})
+export class EditprofileComponent {
+
+}
